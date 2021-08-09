24d2bb3769f9a3ec3429a42870bb6d61fb4cd90e
Robbie Armstrong <34013278+roarmstrong@users.noreply.github.com>
2021-08-10 09:26:59 +1000

    Add small sleep to bumper stop

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ def main():
     with Serial("/dev/ttyUSB0", 115200) as port:
         kobuki = Kobuki(port)
         while True:
+            print(f"x: {kobuki.state.x:.10f}m y: {kobuki.state.y:.10f}m θ: {kobuki.state.theta:.10f}rad")
             if kobuki.state.bumper_activated:
                 kobuki.send_command(stop)
+                sleep(0.5)
             else:
-                print(f"x: {kobuki.state.x:.10f}m y: {kobuki.state.y:.10f}m θ: {kobuki.state.theta:.10f}rad")
                 sleep(0.5)
                 # do something else! e.g.
                 # kobuki.send_command(spin_ccw)
